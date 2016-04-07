f3b4f12c6460b7945ad33309b918a0d4dd12400c
Lorenzo De Angelis <deangelis@amolf.nl>
2016-04-07 10:38:03 +0200

    g(r) function imported from other code

--- g2V.py
+++ g2V.py
@@ -3,6 +3,130 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import time
 
+def pair_correlation_function_2D(kind, x, y, sign, S, rMax, dr): 
+    """
+    Compute the two-dimensional pair correlation function, also known 
+    as the radial distribution function, for a set of circular particles 
+    contained in a square region of a plane.  This simple function finds 
+    reference particles such that a circle of radius rMax drawn around the 
+    particle will fit entirely within the square, eliminating the need to 
+    compensate for edge effects.  If no such particles exist, an error is 
+    returned. Try a smaller rMax...or write some code to handle edge effects! ;) 
+
+    Arguments: 
+        x               an array of x positions of centers of particles 
+        y               an array of y positions of centers of particles 
+        S               length of each side of the square region of the plane 
+        rMax            outer diameter of largest annulus 
+        dr              increment for increasing radius of annulus 
+	
+    Returns a tuple: (g, radii, interior_indices) 
+        g(r)            a numpy array containing the correlation function g(r) 
+        radii           a numpy array containing the radii of the 
+                        annuli used to compute g(r) 
+                        reference_indices   indices of reference particles 
+    """ 
+    # Number of particles in ring/area of ring/number of reference particles/number density 
+    # area of ring = pi*(r_outer**2 - r_inner**2) 
+    
+    
+    # Find particles which are close enough to the box center that a circle of radius 
+    # rMax will not cross any edge of the box 
+    bools1 = x > rMax 
+    bools2 = x < (S - rMax) 
+    bools3 = y > rMax 
+    bools4 = y < (S - rMax) 
+    interior_indices, = np.where(bools1 * bools2 * bools3 * bools4) 
+    num_interior_particles = len(interior_indices) 
+    
+    
+    if num_interior_particles < 1: 
+        raise  RuntimeError ("No particles found for which a circle of radius rMax will lie entirely within a square of side length S.  Decrease rMax or increase the size of the square.") 
+
+
+    edges = np.arange(0., rMax + 1.1 * dr, dr) 
+    num_increments = len(edges) - 1 
+    g = np.zeros([num_interior_particles, num_increments]) 
+    radii = np.zeros(num_increments) 
+    numberDensity = len(x) / np.double(S**2)
+    theta = np.pi/8
+    slice_angle = 2*theta
+    ang_frac = np.pi/slice_angle
+    
+    # Compute pairwise correlation for each interior particle 
+    for p in range(num_interior_particles): 
+        index = interior_indices[p]
+        
+        d = np.sqrt((x[index] - x)**2 + (y[index] - y)**2)
+        d[index] = 2 * rMax
+
+        # Here I calculate the angle of the vector between the two particles
+        # I'm here considering simmetry along x and y axis
+        # By only using the absolute values of the distances
+        if((kind == 'unsigned_y')|(kind == 'signed_y')):
+            angles = np.arctan2(np.abs(x[index] - x),np.abs(y[index] - y))
+        else:
+            angles = np.arctan2(np.abs(y[index] - y),np.abs(x[index] - x))            
+        # And I select only the ones that are within a certain angle theta
+        angular_selection = np.where(angles < theta)
+        d_angular = d[angular_selection]        
+        
+        if(kind == 'unsigned'):
+            (result, bins) = np.histogram(d, bins=edges, density=False) 
+            g[p, :] = result/numberDensity
+        elif((kind == 'unsigned_x')|(kind == 'unsigned_y')):
+            (result,bins) = np.histogram(d_angular, bins = edges, density = False)
+            g[p, :] = (result/numberDensity)*ang_frac
+            
+        else:
+            sign_product = sign[index]*sign
+            
+            pos_indices = np.where(sign_product == 1)
+            neg_indices = np.where(sign_product == -1)
+            
+            d_pos = d[pos_indices]
+            d_neg = d[neg_indices]
+
+            (pos_result, pos_bins) = np.histogram(d_pos, bins=edges, density=False)
+            (neg_result, neg_bins) = np.histogram(d_neg, bins=edges, density=False)
+            
+
+            if(kind == 'signed'):
+                g[p, :] = (pos_result-neg_result)/numberDensity
+            elif(kind == 'same_sign'):
+                numberDensity = len(d_pos) / np.double(S**2)
+                g[p, :] = pos_result/numberDensity
+            elif(kind == 'opp_sign'):
+                numberDensity = len(d_neg) / np.double(S**2)
+                g[p, :] = neg_result/numberDensity
+            elif((kind == 'signed_x')|(kind == 'signed_y')):
+                # For the angular case
+                pos_angular_indices = np.where((sign_product == 1)&(angles < theta))
+                neg_angular_indices = np.where((sign_product == -1)&(angles < theta))
+
+                d_angular_pos = d[pos_angular_indices]
+                d_angular_neg = d[neg_angular_indices]
+
+                (pos_angular_result, pos_bins) = np.histogram(d_angular_pos, bins=edges, density=False)
+                (neg_angular_result, neg_bins) = np.histogram(d_angular_neg, bins=edges, density=False)                        
+            
+                g[p, :] = ((pos_angular_result-neg_angular_result)/numberDensity)*ang_frac
+            
+            else:
+                sys.exit('Error! ' + kind + ' g(r): definition not found.')
+
+    # Average g(r) for all interior particles and compute radii 
+    g_average = np.zeros(num_increments) 
+    for i in range(num_increments): 
+        radii[i] = (edges[i] + edges[i+1]) / 2. 
+        rOuter = edges[i + 1] 
+        rInner = edges[i] 
+        g_average[i] = np.mean(g[:, i]) / (np.pi * (rOuter**2 - rInner**2)) 
+	
+	
+    return (g_average, radii) 
+    
+    
 def get_g(file_name):
     """Gets the radial distribution function from file_name"""
     
