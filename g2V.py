std on the average corrected also in savetxt of g

g2V.py
@@ -471,8 +471,8 @@ def calc_dist_average(g_list, r, name, iteration):
     N_av = len(g_list)
     g_array = np.array(g_list)
     g_av = np.average(g_array, axis = 0)
-    g_std = np.std(g_array,axis = 0)
-    np.savetxt(out_root + 'iters_output/%s_av_%03d.txt' % (name,iteration),np.transpose([r,g_av,g_std]), fmt = '%.5e', delimiter = '\t', header = 'r\tg(r)\tsigma(g)')
+    g_std = np.std(g_array,axis = 0) / np.sqrt(N_av)    ## Error on the average
+    np.savetxt(out_root + 'iters_output/%s_av_%03d.txt' % (name,iteration),np.transpose([r,g_av,g_std]), fmt = '%.5e', delimiter = '\t\t', header = 'r\tg(r)\tsigma(g)')
     
     plt.figure(figsize = (6,4.5))
     #Plot theory
@@ -480,7 +480,7 @@ def calc_dist_average(g_list, r, name, iteration):
         plt.plot(g_th_r,g_th,label = 'expected', zorder = 0)
     elif name == 'S':
         plt.plot(g_th_r, g_th * 4 * np.pi * g_th_r**2,label = 'expected', zorder = 0)
-    plt.errorbar(r,g_av,yerr = g_std / np.sqrt(N_av), marker = 'o', markersize = 3, linestyle = 'None', label = 'measured', zorder = 1)
+    plt.errorbar(r,g_av,yerr = g_std, marker = 'o', markersize = 3, linestyle = 'None', label = 'measured', zorder = 1)
     plt.xlabel(r'$r$')
     plt.ylabel(r'$%s(r)$' % name)
     plt.figtext(0.99, 0.99, git_v, fontsize = 8, ha = 'right', va = 'top')
@@ -535,13 +535,13 @@ if __name__ == '__main__':
     # Monte Carlo Step at which I have convergence
     MC_par['N_conv'] = 50000
     # MC steps to wait between saving observable
-    MC_par['N_corr'] = 2000
+    MC_par['N_corr'] = 2500
     # Initialization of the particles in the box
     MC_par['init_conf'] = 'array_w_noise'
     
     PR_par = {}
     # Number of iterations of Potential retrieval alghoritm
-    PR_par['N_iter'] = 50
+    PR_par['N_iter'] = 60
     PR_par['damping'] = 2.0
 
     